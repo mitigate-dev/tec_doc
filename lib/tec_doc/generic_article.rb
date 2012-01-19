38f821ef5796611e65284831a5eb0d16a2f67950
Ingus Skaistkalns <ingus.skaistkalns@makit.lv>
2012-01-19 11:06:41 +0200

    Generic article search

--- lib/tec_doc/generic_article.rb
+++ lib/tec_doc/generic_article.rb
@@ -1,17 +1,42 @@
 module TecDoc
   class GenericArticle
-    attr_accessor :id
+    attr_accessor :id, :name
+    attr_accessor :scope
     
     # getGenericArticlesByManufacturer5()
-    # getGenericArticlesByManufacturer6()
     
-    def initialize(attributes = {})
-      @id = attributes[:id]
+    # Get generic articles
+    #
+    # @option options [Integer] :assembly_group_node_id (optional)
+    # @option options [LongList] :generic_article_id (optional)
+    # @option options [LongList] :brand_no result of brand selection (optional)
+    # @option options [String] :linking_target_type - "C", "M", "A", "K", "U"
+    # @option options [Integer] :linking_target_id - null if "U"
+    # @option options [Integer] :result_mode - 1: Distinct brand numbers, 2: Distinct generic articles, 3: Both (optional)
+    # @option options [Integer] :sort_mode - 1: Brand name, 2: Article norm name (optional)
+    # @return [Array<TecDoc::GenericArticle>] list of generic articles
+    def self.all(options = {})
+      options = {
+        :lang => I18n.locale.to_s,
+        :country => TecDoc.client.country
+      }.merge(options)
+      TecDoc.client.request(:get_generic_articles_by_manufacturer6, options).map do |attributes|
+        new(attributes, options)
+      end
     end
     
+    def initialize(attributes = {}, scope = {})
+      @id    = (attributes[:id] || attributes[:generic_article_id])
+      @name  = attributes[:article_norm_name]
+      @scope = scope
+    end
+
     # Generic article linked articles
     def articles(options = {})
-      options.merge!({
+      options = {
+        :linking_target_type => scope[:linking_target_type],
+        :linking_target_id   => scope[:linking_target_id]
+      }.merge(options).merge({
         :generic_article_id => { :array => { :id => [id] } }
       })
       Article.all(options)
--- spec/tec_doc/generic_article_spec.rb
+++ spec/tec_doc/generic_article_spec.rb
@@ -1,13 +1,29 @@
+#encoding: utf-8
 require "spec_helper"
 
 describe TecDoc::GenericArticle do
-  it "should return all vehicles articles" do
+  before do
+    VCR.use_cassette("generic_article_all") do
+      @generic_articles = TecDoc::GenericArticle.all({
+        :linking_target_type => "C",
+        :linking_target_id => 10502,
+        :generic_article_id => { :array => { :id => [7] } },
+        :result_mode => 2
+      })
+    end
+  end
+    
+  context ".all" do
+    it "should return distinct generic articles" do
+      @generic_articles.count.should == 1
+      @generic_articles.first.name.should == "Eļļas filtrs"
+    end
+  end
+  
+  it "should return all vehicles articles without any options if scope is present" do
     generic_article = TecDoc::GenericArticle.new(:id => 7)
     VCR.use_cassette("article_all") do
-      @articles = generic_article.articles(
-        :linking_target_type => "C",
-        :linking_target_id => 10502
-      )
+      @articles = generic_article.articles
     end
     @articles.count.should == 30
     @articles.map(&:generic_article_id).uniq.should == [ 7 ]
