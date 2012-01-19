a5d7999305f6c7aaadb4c192589aa3ac93dbfb5e
Edgars Beigarts <edgars.beigarts@makit.lv>
2012-01-19 13:10:05 +0200

    TecDoc logger

--- lib/tec_doc/client.rb
+++ lib/tec_doc/client.rb
@@ -14,13 +14,31 @@ module TecDoc
     end
 
     def request(operation, options = {})
-      response = connection.request(operation) do
-        soap.body = { :in => { :provider => provider }.merge(options) }
+      log operation, options do
+        response = connection.request(operation) do
+          soap.body = { :in => { :provider => provider }.merge(options) }
+        end
+        response.doc.xpath("//data/array/array").map do |node|
+          node_to_hash(node)
+        end
+        # response
       end
-      response.doc.xpath("//data/array/array").map do |node|
-        node_to_hash(node)
-      end
-      # response
+    end
+
+    # Sets the logger to use.
+    attr_writer :logger
+
+    # Returns the logger. Defaults to an instance of +Logger+ writing to STDOUT.
+    def logger
+      @logger ||= ::Logger.new STDOUT
+    end
+
+    def log(operation, options)
+      t = Time.now
+      results = yield
+      duration = 1000.0 * (Time.now - t)
+      logger.info "TecDoc: #{operation.inspect} #{options.inspect} #{'(%.1fms)' % duration}"
+      results
     end
 
     private
